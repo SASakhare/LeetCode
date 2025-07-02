a476510005c73a0f4500a22d2ff6b522bf1a47f7
SASakhare <sakharwsejal@gmail.com>
2025-07-02 13:59:41 +0530

    Time: 0 ms (100%), Space: 15.8 MB (67.14%) - LeetHub

--- 1850-minimum-length-of-string-after-deleting-similar-ends/1850-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/1850-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -6,29 +6,44 @@ public:
         int low=0;
         int high=n-1;
         // int i=0;
-        while(low<high)
-        {
-            // char pref=s[low];
-            int i=low;
-            while(i+1 < n && s[i]==s[i+1])
-                i++;
+        // while(low<high)
+        // {
+        //     // char pref=s[low];
+        //     int i=low;
+        //     while(i+1 < n && s[i]==s[i+1])
+        //         i++;
             
-            int j=high;
-            while( j>0 && s[j-1]==s[j])
-                j--;
+        //     int j=high;
+        //     while( j>0 && s[j-1]==s[j])
+        //         j--;
             
-            if(s[i]==s[j])
-            {
-                low=i+1;
-                high=j-1;
-            }else
-            {
-                break;
-            }
+        //     if(s[i]==s[j])
+        //     {
+        //         low=i+1;
+        //         high=j-1;
+        //     }else
+        //     {
+        //         break;
+        //     }
             
-        }
-        int num=(high-low+1);
-        return num < 0 ? 0 : num;
+        // }
+        // int num=(high-low+1);
+        // return num < 0 ? 0 : num;
         
+
+
+
+         // Another Solution :
+
+
+        while(low<high && s[low]==s[high])
+        {
+            char ch=s[low];
+            while(low<=high && s[low]==ch) low++;
+
+            while(high>=low && s[high]==ch) high--;
+        }
+
+        return high-low+1;
     }
 };
