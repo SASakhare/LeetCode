87b8b89d4189f5d9d0fffbfc9990f1a331ba9d39
SASakhare <sakharwsejal@gmail.com>
2025-07-04 13:39:53 +0530

    Time: 0 ms (100%), Space: 40.3 MB (58.39%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,26 +2,56 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         
+        // it is better one :
+
+        // int n=nums.size();
+        // vector<int>pre(n,1);
+        // vector<int>suff(n,1);
+        // for(int i=1;i<n;i++)
+        // {
+        //     pre[i]=pre[i-1]*nums[i-1];
+        //     suff[n-i-1]=suff[n-i]*nums[n-i];
+        // }
+
+        // // int totalProd=pre[n-1]*nums[n-1];
+
+        // vector<int>res(n);
+        // for(int i=0;i<n;i++)
+        // {
+        //     // res[i]=pre[i]*(totalProd/nums[i]);
+        //     // totalProd/=nums[i];
+
+        //     res[i]=pre[i]*suff[i];
+        // }
+
+        // return res;
+
+
+        // Optimum Approach :
+
         int n=nums.size();
-        vector<int>pre(n,1);
-        vector<int>suff(n,1);
+        vector<int>res(n,1);
+
+        //prefix :
+
+        int pref=1;
         for(int i=1;i<n;i++)
         {
-            pre[i]=pre[i-1]*nums[i-1];
-            suff[n-i-1]=suff[n-i]*nums[n-i];
+            pref=pref*nums[i-1];
+            res[i]=pref;
         }
 
-        // int totalProd=pre[n-1]*nums[n-1];
+        // suffix:
+        int suff=1;
 
-        vector<int>res(n);
-        for(int i=0;i<n;i++)
+        for(int i=n-2;i>=0;i--)
         {
-            // res[i]=pre[i]*(totalProd/nums[i]);
-            // totalProd/=nums[i];
-
-            res[i]=pre[i]*suff[i];
+            suff=suff*nums[i+1];
+            res[i]=res[i]*suff;
         }
+
 
         return res;
+
     }
 };
