98b84c2ecd834e83743f90032f6efb1e2d776531
SASakhare <sakharwsejal@gmail.com>
2025-07-04 13:01:24 +0530

    Time: 85 ms (20.42%), Space: 92 MB (22.67%) - LeetHub

--- 1787-sum-of-absolute-differences-in-a-sorted-array/1787-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/1787-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -1,29 +1,62 @@
 class Solution {
 public:
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+
+
+        // Better :
+
+        // int n=nums.size();
+        // vector<int>pref(n,0);
+        // vector<int>suff(n,0);
+
+        // pref[0]=nums[0];
+        // suff[n-1]=nums[n-1];
+
+        // for(int i=1;i<n;i++)
+        // {
+        //     pref[i]=nums[i]+pref[i-1];
+        //     suff[n-i-1]=nums[n-i-1]+suff[n-i];
+        // }
+
+
+        // vector<int>res(n,0);
+        // for(int i=0;i<n;i++)
+        // {
+        //     int temp=suff[i]-((n-i) * nums[i]);
+        //     if(i>0)
+        //     {
+        //         temp=temp+(nums[i]*i-pref[i-1]);
+        //     }
+        //     res[i]=temp;
+        // }
+
+        // return res;
+
+
+        
         int n=nums.size();
         vector<int>pref(n,0);
         vector<int>suff(n,0);
 
         pref[0]=nums[0];
-        suff[n-1]=nums[n-1];
+        // suff[n-1]=nums[n-1];
 
         for(int i=1;i<n;i++)
         {
             pref[i]=nums[i]+pref[i-1];
-            suff[n-i-1]=nums[n-i-1]+suff[n-i];
+            // suff[n-i-1]=nums[n-i-1]+suff[n-i];
         }
 
 
-        vector<int>res(n,0);
+        vector<int>res(n);
+        int totalSum=pref[n-1];
         for(int i=0;i<n;i++)
         {
-            int temp=suff[i]-((n-i) * nums[i]);
-            if(i>0)
-            {
-                temp=temp+(nums[i]*i-pref[i-1]);
-            }
-            res[i]=temp;
+            int left= i> 0 ? (nums[i] * i) -pref[i-1] : 0;
+
+            int right=totalSum-pref[i]  - nums[i] * (n-i-1);
+
+            res[i]=left+right;
         }
 
         return res;
