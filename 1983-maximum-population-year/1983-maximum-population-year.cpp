6f29610b1b8bdfec3d0345adbd3c7cf4c36e750d
SASakhare <sakharwsejal@gmail.com>
2025-07-05 14:19:35 +0530

    Time: 3 ms (13.1%), Space: 11.3 MB (39.2%) - LeetHub

---
+++ 1983-maximum-population-year/1983-maximum-population-year.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int maximumPopulation(vector<vector<int>>& logs) {
+
+        int n=logs.size();// no of queries.
+        map<int,int>mp;
+        // sort(logs.begin(),logs.end());
+
+        // birth --> 1;
+        // die  ---> -1;
+
+        for(int i=0;i<n;i++)
+        {
+            mp[logs[i][0]]+=1;
+            mp[logs[i][1]]-=1;
+        }
+
+        int sum=0;
+        for(auto it:mp)
+        {
+            sum=sum+it.second;
+            mp[it.first]=sum;
+        }
+
+        // find the year which has max population :
+
+        int year=3000;
+        int pop=0;
+
+        for(auto it :mp)
+        {
+            if(it.second >pop )
+            {
+                year=it.first;
+                pop=it.second;
+            }
+        }
+
+        return year;
+    }
+};
