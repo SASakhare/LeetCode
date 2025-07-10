1344920daa66d918a24f2ab003e8c5bdb31041a3
SASakhare <sakharwsejal@gmail.com>
2025-07-10 12:51:02 +0530

    Time: 2 ms (47.38%), Space: 19.6 MB (12.92%) - LeetHub

--- 1386-shift-2d-grid/1386-shift-2d-grid.cpp
+++ 1386-shift-2d-grid/1386-shift-2d-grid.cpp
@@ -5,26 +5,66 @@ public:
 
         int m=grid.size();
         int n=grid[0].size();
-        while(k>0)
-        {
+        // while(k>0)
+        // {
 
-                int temp1=grid[m-1][n-1];
+        //         int temp1=grid[m-1][n-1];
                 
-                for(int row=0;row<m;row++)
-                {
-                    int temp2=grid[row][n-1];
+        //         for(int row=0;row<m;row++)
+        //         {
+        //             int temp2=grid[row][n-1];
 
-                    for(int col=n-1; col>=1 ;col--)
-                    {
-                        grid[row][col]=grid[row][col-1];
-                    }
+        //             for(int col=n-1; col>=1 ;col--)
+        //             {
+        //                 grid[row][col]=grid[row][col-1];
+        //             }
 
-                    grid[row][0]=temp1;
-                    temp1=temp2;
-                }
+        //             grid[row][0]=temp1;
+        //             temp1=temp2;
+        //         }
         
-            k--;
+        //     k--;
+        // }
+
+        // return grid;
+
+
+        // Optimum :
+        // You can flatten the grid into 1D, shift it efficiently in O(m×n), and then rebuild the grid.
+
+        vector<int>temp;
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                // temp=grid[i][]
+                temp.push_back(grid[i][j]);
+            }
         }
+
+
+
+        // moving circulary :
+
+        k= k%(n*m);
+
+        vector<int>shifted(n*m);
+        for(int i=0;i<n*m;i++)
+        {
+            shifted[(i+k)%(n*m)]=temp[i];
+        }   
+
+        int ind=0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                grid[i][j]=shifted[ind];
+                ind++;
+            }
+        }
+
 
         return grid;
     }
