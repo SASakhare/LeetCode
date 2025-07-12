01904af828aaa8207ac986611a2a8b3d3a767d12
SASakhare <sakharwsejal@gmail.com>
2025-07-12 11:48:56 +0530

    Time: 21 ms (25.69%), Space: 101.1 MB (37.15%) - LeetHub

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/3338-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/3338-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -10,21 +10,31 @@ public:
         for(int i=0;i<n;i++)
         {   
            AllMatrixSum[i][0]=grid[i][0];
-            for(int j=1;j<m;j++)
-            {
-                AllMatrixSum[i][j]=AllMatrixSum[i][j-1]+grid[i][j];
+            int prefix=AllMatrixSum[i][0];
+            for(int j=0;j<m;j++)
+            {   
+                if(j>0)
+                {
+                    AllMatrixSum[i][j]=prefix+grid[i][j];
+                    prefix=AllMatrixSum[i][j];
+                }
+                
+                if(i>0)
+                {
+                    AllMatrixSum[i][j]+=AllMatrixSum[i-1][j];
+                }
             }
         }
 
         // finding the matrix sum:
 
-        for(int i=1;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                    AllMatrixSum[i][j]+=AllMatrixSum[i-1][j];
-            }
-        }
+        // for(int i=1;i<n;i++)
+        // {
+        //     for(int j=0;j<m;j++)
+        //     {
+        //             AllMatrixSum[i][j]+=AllMatrixSum[i-1][j];
+        //     }
+        // }
 
         long long cnt=0;
 
