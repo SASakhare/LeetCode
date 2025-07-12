694cbd55badae78fb44504f621338f18aac507f1
SASakhare <sakharwsejal@gmail.com>
2025-07-12 09:27:27 +0530

    Time: 0 ms (100%), Space: 14.3 MB (42.28%) - LeetHub

--- 1342-queens-that-can-attack-the-king/1342-queens-that-can-attack-the-king.cpp
+++ 1342-queens-that-can-attack-the-king/1342-queens-that-can-attack-the-king.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
 
-    // void fun(vector<vector<int>>&chess,int left,int right,int up,int down,int row,int col,vector<vector<int>>&res)
-    void fun(map<pair<int,int>,bool>&chess,int left,int right,int up,int down,int row,int col,vector<vector<int>>&res)
+    void fun(vector<vector<int>>&chess,int left,int right,int up,int down,int row,int col,vector<vector<int>>&res)
+    // void fun(map<pair<int,int>,bool>&chess,int left,int right,int up,int down,int row,int col,vector<vector<int>>&res)
     {   
         int i=row;
         int j=col;
@@ -29,8 +29,8 @@ public:
             }
 
 
-            // if(i>=0 && i<=8 && j>=0 && j<8 && chess[i][j]==-1)
-            if(i>=0 && i<=8 && j>=0 && j<8 && chess.find({i,j})!=chess.end())
+            if(i>=0 && i<8 && j>=0 && j<8 && chess[i][j]==-1)
+            // if(i>=0 && i<=8 && j>=0 && j<8 && chess.find({i,j})!=chess.end())
             {
                 res.push_back({i,j});
                 return ;
@@ -43,17 +43,17 @@ public:
     }
     vector<vector<int>> queensAttacktheKing(vector<vector<int>>& queens, vector<int>& king) {
         
-        // vector<vector<int>>chess(8,vector<int>(8,0));
-            map<pair<int,int>,bool>chess;
+        vector<vector<int>>chess(8,vector<int>(8,0));
+            // map<pair<int,int>,bool>chess;
         // marking the queen as -1 and king as 1;
 
         for(auto it:queens)
         {
-            // chess[it[0]][it[1]]=1;
-            chess[make_pair(it[0],it[1])]=true;
+            chess[it[0]][it[1]]=-1;
+            // chess[make_pair(it[0],it[1])]=true;
         }
 
-        // chess[king[0]][king[1]]=-1;
+        chess[king[0]][king[1]]=1;
 
 
         vector<int>r={-1,0,1};
