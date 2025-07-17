783f095f007228ace5f3129037c386381feddfde
SASakhare <sakharwsejal@gmail.com>
2025-07-17 10:35:53 +0530

    Time: 391 ms (70.74%), Space: 119.3 MB (45.36%) - LeetHub

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/1557-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/1557-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -2,43 +2,68 @@ class Solution {
 public:
     bool hasAllCodes(string s, int k) {
 
+
+        // Better :
+
+    //     int n=s.size();
+    //     vector<int>mp(pow(2,k),0);
+
+    //     int low=0;
+    //     int high=k-1;
+
+
+    //     while(low<=high && high<n)
+    //     {
+
+    //         long long value=0; // max value of k is 20 (given)
+    //         int pw=0;
+    //         for(int i = high ;i >= low ; i--)
+    //         {
+    //             value+=(s[i]=='1' ? pow(2,pw):0);
+    //             pw++;
+    //         }
+
+    //         mp[value]=1;// mark this no is present 
+
+    //         low++;
+    //         high++;
+
+    //     }
+
+    // // iterating to mp  checking for is all code's present;
+
+    //     int len=pow(2,k);
+    //     for(int i=0;i<len;i++)
+    //     {
+    //         if(mp[i]==0)
+    //         {
+    //             return false;
+    //         }
+    //     }
+
+    //     return true;
+
+            // Optimum :
+
+
+        int len=pow(2,k);
         int n=s.size();
-        vector<int>mp(pow(2,k),0);
+
+        unordered_set<string>codeset;
 
         int low=0;
         int high=k-1;
 
-
         while(low<=high && high<n)
         {
+            // taking out the substring which start at low and end at high:
 
-            long long value=0; // max value of k is 20 (given)
-            int pw=0;
-            for(int i = high ;i >= low ; i--)
-            {
-                value+=(s[i]=='1' ? pow(2,pw):0);
-                pw++;
-            }
-
-            mp[value]=1;// mark this no is present 
-
+            codeset.insert(s.substr(low,high-low+1));
             low++;
             high++;
-
         }
 
-    // iterating to mp  checking for is all code's present;
 
-        int len=pow(2,k);
-        for(int i=0;i<len;i++)
-        {
-            if(mp[i]==0)
-            {
-                return false;
-            }
-        }
-
-        return true;
-        
+        return len==codeset.size();        
     }
 };
