6c434f445bf3687d2243349b749b9a9a0c60cbd8
SASakhare <sakharwsejal@gmail.com>
2025-07-17 11:55:04 +0530

    Time: 4 ms (69.69%), Space: 134.2 MB (84.6%) - LeetHub

--- 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.cpp
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.cpp
@@ -4,27 +4,54 @@ public:
         
         int n=nums.size();
         vector<int>res(n,-1);
-        vector<long long>pref(n,0);
-        pref[0]=nums[0];
-        for(int i=1;i<n;i++)
+
+        if(n<(2*k+1))
         {
-            pref[i]=pref[i-1]+nums[i];
+            return res;
+        }
+        // vector<long long>pref(n,0);
+        // pref[0]=nums[0];
+        // for(int i=1;i<n;i++)
+        // {
+        //     pref[i]=pref[i-1]+nums[i];
+        // }
+
+        // for(int i=0;i<n;i++)
+        // {
+        //     int low=i-k;
+        //     int high=i+k;
+        //     // valid range check :
+        //     if(low>=0 && high<n)
+        //     {
+        //         //low sum prefix[i-1] and high sum prefix[i+k]-prefix[i];
+        //         long long lowSum=pref[i] - (i-k>0 ? pref[i-k-1] : 0);
+        //         long long  highSum=pref[i+k]-pref[i];
+        //         res[i]=(lowSum+highSum)/(2*k+1);
+        //     }
+        // }
+        
+        // return res;
+
+        long long len=2*k+1;
+        long long sum=0;
+
+        for(int i=0;i<len;i++)
+        {
+            sum+=nums[i];
         }
 
-        for(int i=0;i<n;i++)
+        // checking the center k is valid or not:
+        res[k]=(sum/len);
+
+        // valid range :
+        for(int i=k+1;i<=n-k-1;i++)
         {
-            int low=i-k;
-            int high=i+k;
-            // valid range check :
-            if(low>=0 && high<n)
-            {
-                //low sum prefix[i-1] and high sum prefix[i+k]-prefix[i];
-                long long lowSum=pref[i] - (i-k>0 ? pref[i-k-1] : 0);
-                long long  highSum=pref[i+k]-pref[i];
-                res[i]=(lowSum+highSum)/(2*k+1);
-            }
+            sum-=nums[i-k-1];
+            sum+=nums[i+k];
+
+            res[i]=(sum/len);
         }
-        
+
         return res;
     }
 };
