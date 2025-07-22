c357555f9721051372c0c706eccb61de7778c812
SASakhare <sakharwsejal@gmail.com>
2025-07-22 07:44:19 +0530

    Time: 1 ms (49.92%), Space: 10.3 MB (27.23%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int fun(vector<int>&nums, int l,int r)
+    int fun(vector<int>&nums, int l,int r,vector<vector<int>>&dp)
     {
         if(l>r) return 0;
-        int left=nums[l] - fun(nums,l+1,r);
-        int right=nums[r]-fun(nums,l,r-1);
-        return max(left,right);
+
+        if(dp[l][r]!=INT_MIN)
+        {
+            return dp[l][r];
+        }
+        int left=nums[l] - fun(nums,l+1,r,dp);
+        int right=nums[r]-fun(nums,l,r-1,dp);
+        return dp[l][r] = max(left,right);
     }
     bool predictTheWinner(vector<int>& nums) {
         
         int n=nums.size();
-        int l=0;
-        int r=0;
-
-        return fun(nums,0,n-1) >=0;
+        vector<vector<int>> dp(n,vector<int>(n,INT_MIN));
+        return fun(nums,0,n-1,dp) >=0;
     }
 };
