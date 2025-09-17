68cc0f10cd4102963c697db241654693d6047d4d
SASakhare <sakharwsejal@gmail.com>
2025-09-17 12:50:46 +0530

    Time: 350 ms (23.15%), Space: 21.1 MB (77.02%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,43 +1,82 @@
+// class Solution {
+// public:
+//     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+
+//         unordered_set<string>st(wordList.begin(),wordList.end());
+//         queue<pair<string,int>> q;
+//         q.push(make_pair(beginWord,1));
+//         st.erase(beginWord);
+
+//         while(!q.empty())
+//         {
+//             string word=q.front().first;
+//             int cnt=q.front().second;
+//             q.pop();
+
+//             if(word==endWord)
+//             {
+//                 return cnt;
+//             }
+
+//             int n=word.size();
+//             for(int i=0;i<n;i++)
+//             {
+//                 char original=word[i];
+//                 for(char ch='a';ch<='z';ch++)
+//                 {
+//                     word[i]=ch;
+//                     if(st.find(word)!=st.end())
+//                     {
+//                         q.push(make_pair(word,cnt+1));
+//                         st.erase(word);
+//                     }
+
+//                 }
+
+//                 word[i]=original;
+//             }
+
+//         }
+
+//         return 0;
+//     }
+// };
+
+
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-
-        unordered_set<string>st(wordList.begin(),wordList.end());
-        queue<pair<string,int>> q;
-        q.push(make_pair(beginWord,1));
-        st.erase(beginWord);
+        
+        set<string>s(wordList.begin(),wordList.end());
+        s.erase(beginWord);
+        queue<pair<string,int>>q;
+        q.push({beginWord,1});
 
         while(!q.empty())
         {
             string word=q.front().first;
-            int cnt=q.front().second;
+            int steps=q.front().second;
             q.pop();
 
-            if(word==endWord)
-            {
-                return cnt;
-            }
-
+            if(word==endWord) return steps;
             int n=word.size();
             for(int i=0;i<n;i++)
             {
-                char original=word[i];
-                for(char ch='a';ch<='z';ch++)
+                char orig=word[i];
+
+                for(char ch='a' ;ch<='z';ch++)
                 {
                     word[i]=ch;
-                    if(st.find(word)!=st.end())
+                    if(s.find(word)!=s.end())
                     {
-                        q.push(make_pair(word,cnt+1));
-                        st.erase(word);
+                        q.push({word,steps+1});
+                        s.erase(word);
                     }
-
                 }
-
-                word[i]=original;
+                word[i]=orig;
             }
-
         }
-
+        
         return 0;
     }
 };
